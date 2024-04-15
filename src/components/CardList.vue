19082cfdf22f2da428f7b17dc9a7ef13c4853ed9
Konstantin <sterxovK@mail.ru>
2024-04-15 13:38:12 +0400

    hotFix sort

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,9 +1,21 @@
 <template>
   <section
-    :style="`background: ${options.color}`"
-    @drop="onDrop($event, options.id)"
-    @dragover.prevent
-    @dragenter.prevent>
+      :style="`background: ${options.color}`"
+      @drop="onDrop($event, options.id)"
+      @dragover.prevent
+      @dragenter.prevent>
+    <v-tooltip :text="`${text}`">
+      <template v-slot:activator="{ props }">
+        <v-btn
+            v-bind="props"
+            icon="mdi-sort"
+            density="compact"
+            variant="tonal"
+            class="sort-btn align-self-end"
+            color="blue"
+            @click="toggleSortOrder"/>
+      </template>
+    </v-tooltip>
     <div class="title">
       <h2>
         {{ options.title }}
@@ -13,151 +25,174 @@
       </div>
     </div>
     <v-btn
-      icon="mdi-plus"
-      variant="tonal"
-      class="mt-5"
-      color="white"
-      @click="isNewCardDialogOpen = true" />
+        icon="mdi-plus"
+        variant="tonal"
+        class="mt-5"
+        color="white"
+        @click="isNewCardDialogOpen = true"/>
 
     <CardItem
-      v-for="(card, index) in cards"
-      draggable="true"
-      :key="index"
-      :card="card"
-      :options="props.options"
-      @delete-card="deleteCard(card.id)"
-      @dragstart="onDragStart($event, card)" />
+        v-for="(card, index) in cards"
+        draggable="true"
+        :key="index"
+        :card="card"
+        :options="props.options"
+        @delete-card="deleteCard(card.id)"
+        @dragstart="onDragStart($event, card)"/>
 
     <CardForm
-      title="Добавление новой карточки"
-      v-model="isNewCardDialogOpen"
-      :form="form"
-      @save-card="addCard"
-      @close-form="isNewCardDialogOpen = false" />
+        title="Добавление новой карточки"
+        v-model="isNewCardDialogOpen"
+        :form="form"
+        @save-card="addCard"
+        @close-form="isNewCardDialogOpen = false"/>
   </section>
 </template>
 
 <script setup>
-  import { ref, inject } from 'vue';
-  import CardItem from './CardItem.vue';
-  import CardForm from './CardForm.vue';
+import {inject, ref} from 'vue';
+import CardItem from './CardItem.vue';
+import CardForm from './CardForm.vue';
 
-  const firstList = inject('firstList');
-  const secondList = inject('secondList');
-  const lastList = inject('lastList');
+const firstList = inject('firstList');
+const secondList = inject('secondList');
+const lastList = inject('lastList');
 
-  const props = defineProps({
-    options: {},
-  });
+const props = defineProps({
+  options: {},
+});
 
-  const isNewCardDialogOpen = ref(false);
+const isNewCardDialogOpen = ref(false);
+const sortOrder = ref('ascending');
+const text = ref('по убывнию рейтинга')
+const form = ref({
+  image: '',
+  id: null,
+  title: '',
+  description: '',
+  category: '',
+  price: null,
+  rating: {},
+});
 
-  const form = ref({
-    image: '',
-    id: null,
-    title: '',
-    description: '',
-    category: '',
-    price: null,
-    rating: {},
-  });
+let cards = ref([]);
 
-  let cards = ref([]);
-
-  function getLocalCards() {
-    switch (props.options.id) {
-      case 1:
-        cards = firstList;
-        break;
-      case 2:
-        cards = secondList;
-        break;
-      case 3:
-        cards = lastList;
-        break;
-    }
+function getLocalCards() {
+  switch (props.options.id) {
+    case 1:
+      cards = firstList;
+      break;
+    case 2:
+      cards = secondList;
+      break;
+    case 3:
+      cards = lastList;
+      break;
   }
-  getLocalCards();
+}
 
-  function addCard() {
-    cards.value.unshift(form.value);
-    isNewCardDialogOpen.value = false;
+getLocalCards();
+
+function addCard() {
+  cards.value.unshift(form.value);
+  isNewCardDialogOpen.value = false;
+}
+
+function deleteCard(id) {
+  cards.value = cards.value.filter((card) => card.id != id);
+}
+
+function onDragStart(event, item) {
+  event.dataTransfer.dropEffect = 'move';
+  event.dataTransfer.effectAllowed = 'move';
+  event.dataTransfer.setData('itemId', item.id.toString());
+}
+
+const toggleSortOrder = () => {
+  if (sortOrder.value === 'ascending') {
+    sortOrder.value = 'descending';
+    text.value = 'по возрастанию рейтинга'
+    return cards.value.sort((a, b) => b.rating.rate - a.rating.rate)
+  } else if (sortOrder.value === 'descending') {
+    sortOrder.value = 'neutral';
+    text.value = 'без сортировки по рейтингу'
+    return cards.value.sort((a, b) => a.rating.rate - b.rating.rate)
+  } else {
+    sortOrder.value = 'ascending';
+    text.value = 'по убывнию рейтинга'
+    return cards.value.sort((a, b) => a.id - b.id)
   }
-  function deleteCard(id) {
-    cards.value = cards.value.filter((card) => card.id != id);
+};
+
+function onDrop(event, optionsId) {
+  const itemId = parseInt(event.dataTransfer.getData('itemId'));
+  let otherLists = [];
+
+  switch (optionsId) {
+    case 1:
+      cards.value = firstList.value;
+      otherLists = secondList.value.concat(lastList.value);
+      break;
+    case 2:
+      cards.value = secondList.value;
+      otherLists = firstList.value.concat(lastList.value);
+      break;
+    case 3:
+      cards.value = lastList.value;
+      otherLists = secondList.value.concat(firstList.value);
+      break;
   }
-  function onDragStart(event, item) {
-    event.dataTransfer.dropEffect = 'move';
-    event.dataTransfer.effectAllowed = 'move';
-    event.dataTransfer.setData('itemId', item.id.toString());
+
+  const newCard = otherLists.find((card) => card.id === itemId);
+
+  firstList.value = firstList.value.filter((card) => card.id !== newCard.id);
+  secondList.value = secondList.value.filter((card) => card.id !== newCard.id);
+  lastList.value = lastList.value.filter((card) => card.id !== newCard.id);
+  cards.value.unshift(newCard);
+
+  switch (optionsId) {
+    case 1:
+      firstList.value = cards.value;
+      break;
+    case 2:
+      secondList.value = cards.value;
+      break;
+    case 3:
+      lastList.value = cards.value;
+      break;
   }
-  function onDrop(event, optionsId) {
-    const itemId = parseInt(event.dataTransfer.getData('itemId'));
-    let otherLists = [];
-
-    switch (optionsId) {
-      case 1:
-        cards.value = firstList.value;
-        otherLists = secondList.value.concat(lastList.value);
-        break;
-      case 2:
-        cards.value = secondList.value;
-        otherLists = firstList.value.concat(lastList.value);
-        break;
-      case 3:
-        cards.value = lastList.value;
-        otherLists = secondList.value.concat(firstList.value);
-        break;
-    }
-
-    const newCard = otherLists.find((card) => card.id === itemId);
-
-    firstList.value = firstList.value.filter((card) => card.id !== newCard.id);
-    secondList.value = secondList.value.filter((card) => card.id !== newCard.id);
-    lastList.value = lastList.value.filter((card) => card.id !== newCard.id);
-    cards.value.unshift(newCard);
-
-    switch (optionsId) {
-      case 1:
-        firstList.value = cards.value;
-        break;
-      case 2:
-        secondList.value = cards.value;
-        break;
-      case 3:
-        lastList.value = cards.value;
-        break;
-    }
-  }
+}
 </script>
 
 <style lang="scss" scoped>
-  section {
-    padding: 10px;
-    width: 400px;
-    min-height: 500px;
-    height: fit-content;
-    border-radius: 10px;
+section {
+  padding: 10px;
+  width: 400px;
+  min-height: 500px;
+  height: fit-content;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .title {
+    padding-bottom: 10px;
+    width: 90%;
     display: flex;
-    flex-direction: column;
     align-items: center;
-    .title {
-      padding-bottom: 10px;
-      width: 90%;
+    justify-content: center;
+    border-bottom: 2px solid white;
+
+    .counter {
+      margin-left: 10px;
+      background: white;
+      border-radius: 50%;
+      width: 30px;
+      height: 30px;
       display: flex;
-      align-items: center;
       justify-content: center;
-      border-bottom: 2px solid white;
-      .counter {
-        margin-left: 10px;
-        background: white;
-        border-radius: 50%;
-        width: 30px;
-        height: 30px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
+      align-items: center;
     }
   }
+}
 </style>
